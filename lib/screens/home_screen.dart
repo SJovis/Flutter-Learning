22c4c33991ae73d16745ef766e774ae4f5dab77e
Sérgio Assis Cerqueira <jovis.sergio@gmail.com>
2022-09-14 18:49:44 +0100

    added search box and second heading

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import '../utils/app_styles.dart';
@@ -46,6 +47,37 @@ class HomeScreen extends StatelessWidget {
                         ),
                       ),
                     ),
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                          color: Color(0xFFBFC205)),
+                      Text('Search', style: Styles.headlineStyle4)
+                    ],
+                  ),
+                ),
+                const Gap(30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Upcomging Flights', style: Styles.headlineStyle2),
+                    InkWell(
+                      onTap: () {
+                        print('You are tapped');
+                      },
+                      child: Text('View all',
+                          style: Styles.textStyle
+                              .copyWith(color: Styles.primaryColor)),
+                    )
                   ],
                 )
               ],
